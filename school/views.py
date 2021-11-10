5b5dc24824f57d96f3b644a774a3de8ccb735b1f
denis burnashev <kardenman@gmail.com>
2021-11-10 12:00:40 +0600

    origin

---
+++ school/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student, Teacher
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students = Student.objects.all()
+    for each in students:
+        context = {'object_list': students, 'student.teacher.all': each.teacher.all()}
+
+    # используйте этот параметр для упорядочивания результатов
+    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
+    ordering = 'group'
+
+    return render(request, template, context)
